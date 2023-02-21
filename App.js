4adda8f587b4e4cd16103d8a0a2066d0f901c826
Adinnu Benedict <24754078+BenQoder@users.noreply.github.com>
2023-02-21 14:29:10 +0100

    Expo build

--- App.js
+++ App.js
@@ -11,9 +11,10 @@ import * as Contacts from "expo-contacts";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Linking from "expo-linking";
-import Spacer from "../components/spacer";
+import Spacer from "./components/spacer";
 import Modal from "react-native-modal";
 import parsePhoneNumber from "libphonenumber-js";
+import Providers from "./Providers";
 
 export default function Page() {
   const anonKey = process.env.ANON_KEY;
@@ -64,6 +65,8 @@ export default function Page() {
             }),
           }
         );
+
+        console.log(await response.json());
       }
 
       setIsLoading(false);
@@ -85,192 +88,199 @@ export default function Page() {
     );
 
     setIsLoading(false);
+    const jsonx = await response.json();
+    console.log(jsonx);
 
     if (response.status !== 200) {
       return;
     }
 
     const json = await response.json();
+    console.log(json);
 
     setContact(json.data[0] ?? null);
   };
 
   return (
-    <Layout style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1 }} edges={["top", "bottom"]}>
-        <View
-          style={{
-            // justifyContent: "center",
-            marginTop: 30,
-            flexDirection: "row",
-            // flex: 1,
-            paddingHorizontal: 15,
-            alignItems: "center",
-          }}
-        >
-          <Image
-            source={require("../assets/lp-logo-large.png")}
-            style={{ width: 60, height: 60 }}
-          />
-
-          <Spacer width={5} />
-
-          <View style={{ flex: 1 }}>
-            <Text category="h6">Join the {`\n`}movement</Text>
-          </View>
-
-          {isLoading && <Spinner size="small" status="primary" />}
-        </View>
-
-        <View style={{ paddingHorizontal: 15, marginTop: 30, flex: 1 }}>
+    <Providers>
+      <Layout style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }} edges={["top", "bottom"]}>
           <View
             style={{
-              paddingBottom: 15,
+              // justifyContent: "center",
+              marginTop: 30,
+              flexDirection: "row",
+              // flex: 1,
+              paddingHorizontal: 15,
+              alignItems: "center",
             }}
           >
-            <Text allowFontScaling={false} category="h6">
-              Obi | Datti Messenger
-            </Text>
+            <Image
+              source={require("./assets/lp-logo-large.png")}
+              style={{ width: 60, height: 60 }}
+            />
 
-            <Spacer height={2} />
+            <Spacer width={5} />
 
-            <Text allowFontScaling={false} category="s1">
-              Obidents this is your new campaign messenger, mobilize and remind
-              other Nigerians to vote Labour party.
-            </Text>
+            <View style={{ flex: 1 }}>
+              <Text category="h6">Join the {`\n`}movement</Text>
+            </View>
+
+            {isLoading && <Spinner size="small" status="primary" />}
           </View>
 
-          <Divider style={{}} />
-
-          <ScrollView
-            style={{ flex: 1 }}
-            contentContainerStyle={{ paddingBottom: 80 }}
-          >
+          <View style={{ paddingHorizontal: 15, marginTop: 30, flex: 1 }}>
             <View
               style={{
-                marginTop: 10,
-                overflow: "hidden",
+                paddingBottom: 15,
               }}
             >
-              {canShowModal ? (
-                <>
-                  {!contact && (
-                    <>
-                      <View
-                        style={{
-                          padding: 15,
-                          backgroundColor: "#0f1218",
-                          borderRadius: 5,
-                          marginBottom: 10,
-                          borderWidth: 1,
-                          borderStyle: "dotted",
-                          borderColor: theme["color-primary-500"],
-                        }}
-                      >
-                        <Text appearance="alternative" category="s1">
-                          When you click "Get Contact" Button, you will be
-                          randomly assigned a contact from the database, you can
-                          then send them a campaign message via WhatsApp or SMS.
-                        </Text>
-                        <Spacer height={10} />
-                        <Text style={{ color: "#fff" }} category="s1">
-                          The idea is to reach out to as many Nigerians as
-                          possible, free via WhatsApp or without conventional
-                          SMS Gateways, and to remind them to vote Labour Party
-                          and Join the Obidents Movement.
-                        </Text>
-                      </View>
-                      <Button onPress={getRandomContact} disabled={isLoading}>
-                        Get Contact
-                      </Button>
-                    </>
-                  )}
-                </>
-              ) : (
-                <>
-                  {!contact && (
-                    <>
-                      <View
-                        style={{
-                          padding: 15,
-                          backgroundColor: "#0f1218",
-                          borderRadius: 5,
-                          marginBottom: 10,
-                          borderWidth: 1,
-                          borderStyle: "dotted",
-                          borderColor: theme["color-primary-500"],
-                        }}
-                      >
-                        <Text appearance="alternative" category="s1">
-                          To avoid spamming, Please come back later to get
-                          another contact.
-                        </Text>
-                      </View>
-                    </>
-                  )}
-                </>
-              )}
+              <Text allowFontScaling={false} category="h6">
+                Obi | Datti Messenger
+              </Text>
 
-              <Spacer height={30} />
+              <Spacer height={2} />
 
-              {Platform.OS != "web" && (
-                <>
-                  <View
-                    style={{
-                      padding: 15,
-                      backgroundColor: "#0f1218",
-                      borderRadius: 5,
-                      marginBottom: 10,
-                      borderWidth: 1,
-                      borderStyle: "dotted",
-                      borderColor: theme["color-primary-500"],
-                    }}
-                  >
-                    <Text style={{ color: "#fff" }} category="s1">
-                      By clicking the button below, you will be uploading 50
-                      phone numbers from your phone to the database, The contact
-                      will be randomly assigned to participants of the campaign.
-                    </Text>
-                    <Spacer height={10} />
-                    <Text style={{ color: "#fff" }} category="s1">
-                      Note: This App will not upload your contact names or any
-                      other information, only phone numbers.{`\n`}To avoid
-                      numbers from being assigned to multiple participants, we
-                      will mark the numbers as used after they have been
-                      assigned and all contacts will be deleted after the
-                      election.
-                    </Text>
-                  </View>
-
-                  <Button onPress={uploadContacts} disabled={isLoading}>
-                    Upload Contacts
-                  </Button>
-                </>
-              )}
+              <Text allowFontScaling={false} category="s1">
+                Obidents this is your new campaign messenger, mobilize and
+                remind other Nigerians to vote Labour party.
+              </Text>
             </View>
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-      <Modal
-        hasBackdrop
-        style={{
-          margin: 0,
-          justifyContent: "flex-end",
-          backgroundColor: "rgba(0,0,0,0.7)",
-        }}
-        visible={!!contact}
-        onBackdropPress={() => {
-          setCanShowModal(false);
-          setContact(null);
-        }}
-        onBackButtonPress={() => {
-          setCanShowModal(false);
-          setContact(null);
-        }}
-      >
-        <ContactsModal contact={contact} />
-      </Modal>
-    </Layout>
+
+            <Divider style={{}} />
+
+            <ScrollView
+              style={{ flex: 1 }}
+              contentContainerStyle={{ paddingBottom: 80 }}
+            >
+              <View
+                style={{
+                  marginTop: 10,
+                  overflow: "hidden",
+                }}
+              >
+                {canShowModal ? (
+                  <>
+                    {!contact && (
+                      <>
+                        <View
+                          style={{
+                            padding: 15,
+                            backgroundColor: "#0f1218",
+                            borderRadius: 5,
+                            marginBottom: 10,
+                            borderWidth: 1,
+                            borderStyle: "dotted",
+                            borderColor: theme["color-primary-500"],
+                          }}
+                        >
+                          <Text appearance="alternative" category="s1">
+                            When you click "Get Contact" Button, you will be
+                            randomly assigned a contact from the database, you
+                            can then send them a campaign message via WhatsApp
+                            or SMS.
+                          </Text>
+                          <Spacer height={10} />
+                          <Text style={{ color: "#fff" }} category="s1">
+                            The idea is to reach out to as many Nigerians as
+                            possible, free via WhatsApp or without conventional
+                            SMS Gateways, and to remind them to vote Labour
+                            Party and Join the Obidents Movement.
+                          </Text>
+                        </View>
+                        <Button onPress={getRandomContact} disabled={isLoading}>
+                          Get Contact
+                        </Button>
+                      </>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    {!contact && (
+                      <>
+                        <View
+                          style={{
+                            padding: 15,
+                            backgroundColor: "#0f1218",
+                            borderRadius: 5,
+                            marginBottom: 10,
+                            borderWidth: 1,
+                            borderStyle: "dotted",
+                            borderColor: theme["color-primary-500"],
+                          }}
+                        >
+                          <Text appearance="alternative" category="s1">
+                            To avoid spamming, Please come back later to get
+                            another contact.
+                          </Text>
+                        </View>
+                      </>
+                    )}
+                  </>
+                )}
+
+                <Spacer height={30} />
+
+                {Platform.OS != "web" && (
+                  <>
+                    <View
+                      style={{
+                        padding: 15,
+                        backgroundColor: "#0f1218",
+                        borderRadius: 5,
+                        marginBottom: 10,
+                        borderWidth: 1,
+                        borderStyle: "dotted",
+                        borderColor: theme["color-primary-500"],
+                      }}
+                    >
+                      <Text style={{ color: "#fff" }} category="s1">
+                        By clicking the button below, you will be uploading 50
+                        phone numbers from your phone to the database, The
+                        contact will be randomly assigned to participants of the
+                        campaign.
+                      </Text>
+                      <Spacer height={10} />
+                      <Text style={{ color: "#fff" }} category="s1">
+                        Note: This App will not upload your contact names or any
+                        other information, only phone numbers.{`\n`}To avoid
+                        numbers from being assigned to multiple participants, we
+                        will mark the numbers as used after they have been
+                        assigned and all contacts will be deleted after the
+                        election.
+                      </Text>
+                    </View>
+
+                    <Button onPress={uploadContacts} disabled={isLoading}>
+                      Upload Contacts
+                    </Button>
+                  </>
+                )}
+              </View>
+            </ScrollView>
+          </View>
+        </SafeAreaView>
+        <Modal
+          hasBackdrop
+          style={{
+            margin: 0,
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0,0,0,0.7)",
+          }}
+          visible={!!contact}
+          onBackdropPress={() => {
+            setCanShowModal(false);
+            setContact(null);
+          }}
+          onBackButtonPress={() => {
+            setCanShowModal(false);
+            setContact(null);
+          }}
+        >
+          <ContactsModal contact={contact} />
+        </Modal>
+      </Layout>
+    </Providers>
   );
 }
 
---
+++ Providers.js
@@ -0,0 +1,41 @@
+import { View, Text, Dimensions } from "react-native";
+import React from "react";
+import * as eva from "@eva-design/eva";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import {
+  ApplicationProvider,
+  IconRegistry,
+  Layout,
+} from "@ui-kitten/components";
+import * as theme from "./assets/theme.json";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+
+export default function Providers({ children }) {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider
+        {...eva}
+        theme={{ ...eva.light, ...theme }}
+        // customMapping={mapping}
+      >
+        <SafeAreaProvider>
+          <Layout style={{ flex: 1, backgroundColor: "rgba(255,0,0,0.4)" }}>
+            <Layout
+              style={{
+                maxWidth: 550,
+                marginLeft: "auto",
+                marginRight: "auto",
+                height: Dimensions.get("window").height,
+              }}
+            >
+              {children}
+            </Layout>
+          </Layout>
+        </SafeAreaProvider>
+      </ApplicationProvider>
+    </>
+  );
+}
